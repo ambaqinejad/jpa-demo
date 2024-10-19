8f172dd4fae339dd280bf45c403c6a1fbc16658b
ambaqinejad <ambaqinejad@gamil.com>
2024-10-19 21:13:25 +0330

    wip

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,15 +6,23 @@ import jakarta.persistence.Persistence;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("");
-        EntityManager entityManager = entityManagerFactory.createEntityManager();
+public class App {
+    public static void main(String[] args) {
+        try (EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("pu")) {
+            EntityManager entityManager = entityManagerFactory.createEntityManager();
+            Person p = entityManager.find(Person.class, 1);
+            System.out.println(p);
+            entityManager.getTransaction().begin();
+            Person p2 = new Person();
+            p2.setName("علی");
+            entityManager.persist(p2);
+            entityManager.getTransaction().commit();
 
-        System.out.println( "Hello World!" );
+            p = entityManager.find(Person.class, 2);
+            System.out.println(p);
+        }
+
+        System.out.println("Hello World!");
     }
 }
